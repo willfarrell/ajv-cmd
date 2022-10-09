ab7e970be062338ab1e9620f5551556c471d5f96
will Farrell <willfarrell@proton.me>
2022-10-08 19:22:58 -0600

    fix: ftl export

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {
   instance as instanceImport,
   compile as compileImport
 } from './compile.js'
+import ftlImport from './ftl.js'
 import transpileImport from './transpile.js'
 import validateImport from './validate.js'
 
@@ -9,10 +10,12 @@ export const instance = instanceImport
 export const compile = compileImport
 export const transpile = transpileImport
 export const validate = validateImport
+export const ftl = ftlImport
 
 export default {
   instance,
   compile,
+  ftl,
   transpile,
   validate
 }
