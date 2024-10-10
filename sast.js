57ed5ab51a491c0e0aa94a544de0a87165b3be38
will Farrell <willfarrell@proton.me>
2024-10-10 11:57:27 -0600

    fix: remove warning on sast

--- sast.js
+++ sast.js
@@ -1,21 +1,14 @@
+import { createRequire } from 'node:module'
 import Ajv from 'ajv/dist/2020.js'
 //import sastSchema from 'sast-json-schema/index.json' with { type: 'json' }
-// const sastSchema = await import('sast-json-schema/index.json', {
-//   assert: {
-//     type: 'json'
-//   }
-// })
-import { readFileSync } from 'node:fs'
-
-const __dirname = import.meta.dirname
 
 const defaultOptions = {
   strictTypes: false,
   allErrors: true
 }
 
-const sastSchema = JSON.parse(
-  readFileSync(__dirname + '/node_modules/sast-json-schema/index.json')
+const sastSchema = createRequire(import.meta.url)(
+  './node_modules/sast-json-schema/index.json'
 )
 export const sast = (schema, options = {}) => {
   options = { ...defaultOptions, ...options }
