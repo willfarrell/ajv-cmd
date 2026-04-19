9794e76b3199a511c355653e8d2a06f794509d97
will Farrell <willfarrell@proton.me>
2026-04-18 20:19:17 -0600

    ci: fix lint
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- sast.js
+++ sast.js
@@ -1,11 +1,11 @@
 // Copyright 2026 will Farrell, and ajv-cmd contributors.
 // SPDX-License-Identifier: MIT
 export {
-	MAX_DEPTH,
 	analyze,
 	crawlSchema,
 	isPrivateIP,
+	MAX_DEPTH,
 	resolveSSRFRefs,
 	sast,
+	sast as default,
 } from "sast-json-schema/cli";
-export { sast as default } from "sast-json-schema/cli";
